e812fa22a1a4cd3b9946711d5816aa7f4956dd3b
reJoker <djack0812@gmail.com>
2021-03-15 06:01:51 +0800

    feat(head): update html title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
-        title: 'labeling_official_website',
+        title: 'ZHAI LABHUB',
         meta: [
             { charset: 'utf-8' },
             {
